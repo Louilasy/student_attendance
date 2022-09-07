d67f36a0855ade0e54517690c89e19838522b93f
louilasy <amegashielouis@gmail.com>
2022-09-07 16:17:17 +0000

    Changed the login logo to knust logo

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Louis Amegashie\\Downloads\\student_attendance" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\Louis Amegashie\\Downloads\\student_attendance"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Louis Amegashie\\Downloads\\student_attendance\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Louis Amegashie\\Downloads\\student_attendance"
+  "FLUTTER_TARGET=C:\\Users\\Louis Amegashie\\Downloads\\student_attendance\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Louis Amegashie\\Downloads\\student_attendance\\.dart_tool\\package_config.json"
+)
